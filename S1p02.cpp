f8b96c7dd1cf8dfcab508b821a5ae5bdb1e41bf3
Shagun <shagunpadha108@gmail.com>
2025-09-09 21:55:53 +0530

    Update S1p02.cpp

--- S1p02.cpp
+++ S1p02.cpp
@@ -1,11 +1,11 @@
 // swap two numbers
 #include<iostream>
 using namespace std;
-void swapByValue(int x , int y){
+void swapByValue(int x,int y){
     int temp = x;
     x = y;
     y = temp;
-    cout << "inside swapByValue : a = "<< x <<" and b = "<< y << endl;
+    cout << "inside swapByValue : a = "<<x<<" and b = "<<y<< endl;
 }
 void swapByReference(int* x , int* y){
     int temp = *x;
@@ -17,11 +17,11 @@ void swapByReference(int* x , int* y){
 int main(){
     int a = 10;
     int b = 20;
-    cout << "Before  swap by value : a = " << a << " and b = " << b << endl;
+    cout << "Before swap by value : a = " <<a << " and b = " << b << endl;
     swapByValue(a,b);
-    cout << "After swap by value : a = " << a << " and b =  " << b << endl;
-    cout << endl;
-    cout << "Before swapping  by reference : a = "<< a  << " and b = " << b << endl;
+    cout << "After swap by value : a = " <<a << " and b =  " << b << endl;
+    cout<< endl;
+    cout << "Before swapping  by reference : a = "<< a << " and b = " << b<< endl;
     swapByReference(&a, &b);
-    cout << "After swapping  by reference : a = " << a << " and b = " << b << endl;
+    cout<< "After swapping   by reference : a = " << a << " and b = " << b<< endl;
 }
